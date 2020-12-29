b8e99c847ccbd1ca8b67afe393a99ed25b9df3b7
HaxagonusD <jquezad1@binghamton.edu>
2020-12-29 14:46:05 -0500

    Julian is committing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import "./App.css";
 import FormInput from "./components/FormInput";
-
 import TodoListItem from "./components/TodoListItem";
 import { useState } from "react";
-function App() {
+
+const App = () => {
   const [todos, setTodos] = useState([]);
   return (
-    <div className="App">
-      <h1>Todo List</h1>
-      <FormInput todos={todos} setTodos={setTodos} />
-
-      <div>
-  
+    <div className="input-group mb-3">
+      <div className="flex flex-col justify-center bg-white w-96 mx-auto p-8">
+        <h1 className="text-center">Todo List</h1>
+        <FormInput todos={todos} setTodos={setTodos} />
+      </div>
+      <div className="flex mx-auto">
         {todos.map((task, index) => {
           return <TodoListItem task={task} key={index} />;
         })}
       </div>
+
     </div>
   );
-}
+};
 
 export default App;
